3f86b67d4d5b913f33661694b75fd7b9bd8f54e0
Elmer Marquez <leomarqz2020@gmail.com>
2025-09-21 00:42:55 -0600

    Add project files.

---
+++ StockControlSystem/sql_code.sql
@@ -0,0 +1,27 @@
+
+-- Creamos la base de datos para el sistema de control de existencias.
+CREATE DATABASE StockControlSystem;
+GO
+
+-- Llamamos la base de datos
+USE StockControlSystem;
+
+--Tablas sin dependencias:
+-- * Categories
+-- * Suppliers
+
+--Tablas con dependencias (dependen de las anteriores):
+-- * Products (depende de Categories y Suppliers)
+
+--Tablas con dependencias (dependen de Products):
+-- * InventoryMovements (depende de Products)
+
+-- Creamos la tabla de categorias
+CREATE TABLE Categories (
+    CategoryID INT PRIMARY KEY IDENTITY(1,1),
+    CategoryName NVARCHAR(50) NOT NULL,
+    CreatedDate DATETIME2 NOT NULL DEFAULT GETDATE(),
+    LastModifiedDate DATETIME2 NOT NULL DEFAULT GETDATE(),
+    DeletedDate DATETIME2 NULL
+);
+GO
